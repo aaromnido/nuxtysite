6f3ffd173c40b3bc70981eab7ecdf85d7ea4c420
Fernando Val <aaromnido@gmail.com>
2019-02-19 22:21:57 +0100

    About page partially done

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,176 @@
 <template>
   <section>
-    <h1>About</h1>
+    <div class="hero-second-level">
+      <h4>ABOUT</h4>
+      <h1>
+        Passion. Experience.<br>
+        Diligence.
+      </h1>
+    </div>
+    <div class="section">
+      <div class="content">
+        <div class="row">
+          <div class="column-1-2">
+            <h3>What we do</h3>
+            <p>We build quality products for you fast. Whether you need a complex webapp, mobile app, or just a nice front end or backend, we do it all. We focus on compromising speed with quality and we always want the end user to have a great experience.</p>
+            <p>For new products, our motto is MVP to launch in 3 months with the first iteration in one.</p>
+            <p class="text-center">
+              <nuxt-link to="/portfolio" class="button button-outline">CHECK OUT OUR PORTFOLIO</nuxt-link>
+            </p>
+
+          </div><!-- / column-half -->
+          <div class="column-1-3">
+            <h3>Get in touch</h3>
+            <p>We at Quant Five know that finding the right development team is a choice not to be taken lightly. That’s why we offer free consultations to walk you through your needs, the scope of your goals, and your budget.</p>
+            <p>
+              <a href="https://calendly.com/patrick-lu/phone-chat">BOOK AN APPOINTMENT ▸</a>
+            </p>
+          </div><!-- / column-third -->
+        </div><!-- / row -->
+        <div class="one-centered-column crew">
+
+          <h2>Founder</h2>
+          <div class="row media">
+            <div class="column-1-4">
+              <img src="https://static1.squarespace.com/static/590f9f02ebbd1a0ad377f7ba/t/595ad6421b10e30e6206b958/1499125322499/?format=176w" alt="Patrick Lu" class="img-responsive" width="176">
+            </div>
+            <div class="column-3-4">
+              <h6>CEO / TECH LEAD</h6>
+              <h5>Patrick Lu</h5>
+              <p>I've previously worked for Apple as a software engineer and then went through the YCombinator Fellowship program building out my own businesses. I've built out apps full stack using React, Node, Django, and Java. I know what it takes to build a product from 0 to MVP and then from MVP to scale and I can do it quickly. I've led tech teams as large as 7.</p>
+              <div class="row details">
+                <div class="column-1-3">
+                  <h5>Education</h5>
+                  <p>
+                    <strong>UC Berkeley</strong><br>
+                    Computer Science
+                  </p>
+                </div>
+                <div class="column-2-3">
+                  <h5>Previous Experience</h5>
+                  <ul>
+                    <li>Apple Software Engineer in Machine Learning</li>
+                    <li>YCombinator Fellowship</li>
+                  </ul>
+                </div>
+              </div><!-- / details -->
+            </div>
+          </div><!-- / row -->
+
+          <h2>Cofounder / Engineer</h2>
+          <div class="row media">
+            <div class="column-1-4">
+              <img src="https://static1.squarespace.com/static/590f9f02ebbd1a0ad377f7ba/t/59c31f2664b05fb1d241372e/1505959718275/craig.jpg?format=176w" alt="Patrick Lu" class="img-responsive" width="176">
+            </div>
+            <div class="column-3-4">
+              <h6>CEO / TECH LEAD</h6>
+              <h5>Patrick Lu</h5>
+              <p>I've previously worked for Apple as a software engineer and then went through the YCombinator Fellowship program building out my own businesses. I've built out apps full stack using React, Node, Django, and Java. I know what it takes to build a product from 0 to MVP and then from MVP to scale and I can do it quickly. I've led tech teams as large as 7.</p>
+              <div class="row details">
+                <div class="column-1-3">
+                  <h5>Education</h5>
+                  <p>
+                    <strong>UC Berkeley</strong><br>
+                    Computer Science
+                  </p>
+                </div>
+                <div class="column-2-3">
+                  <h5>Previous Experience</h5>
+                  <ul>
+                    <li>Apple Software Engineer in Machine Learning</li>
+                    <li>YCombinator Fellowship</li>
+                  </ul>
+                </div>
+              </div><!-- / details -->
+            </div>
+          </div><!-- / row -->
+
+          <h2>Cofounder / Engineer</h2>
+          <div class="row media">
+            <div class="column-1-4">
+              <img src="https://static1.squarespace.com/static/590f9f02ebbd1a0ad377f7ba/t/5ada6d2c88251b94cbf3ea8e/1524264240568/joey.jpeg?format=176w" alt="Patrick Lu" class="img-responsive" width="176">
+            </div>
+            <div class="column-3-4">
+              <h6>CEO / TECH LEAD</h6>
+              <h5>Joey Pereira</h5>
+              <p>I've previously worked for Apple as a software engineer and then went through the YCombinator Fellowship program building out my own businesses. I've built out apps full stack using React, Node, Django, and Java. I know what it takes to build a product from 0 to MVP and then from MVP to scale and I can do it quickly. I've led tech teams as large as 7.</p>
+              <div class="row details">
+                <div class="column-1-3">
+                  <h5>Education</h5>
+                  <p>
+                    <strong>UC Berkeley</strong><br>
+                    Computer Science
+                  </p>
+                </div>
+                <div class="column-2-3">
+                  <h5>Previous Experience</h5>
+                  <ul>
+                    <li>Apple Software Engineer in Machine Learning</li>
+                    <li>YCombinator Fellowship</li>
+                  </ul>
+                </div>
+              </div><!-- / details -->
+            </div>
+          </div><!-- / row -->
+          
+          <h2>Software Painter</h2>
+          <div class="row media">
+            <div class="column-1-4">
+              <img src="https://pbs.twimg.com/profile_images/987222302906568704/XzFfyUdb_400x400.jpg" alt="Patrick Lu" class="img-responsive" width="176">
+            </div>
+            <div class="column-3-4">
+              <h6>UX/UI Engineer</h6>
+              <h5>Fernando Val</h5>
+              <p>I've previously worked for Apple as a software engineer and then went through the YCombinator Fellowship program building out my own businesses. I've built out apps full stack using React, Node, Django, and Java. I know what it takes to build a product from 0 to MVP and then from MVP to scale and I can do it quickly. I've led tech teams as large as 7.</p>
+              <div class="row details">
+                <div class="column-1-3">
+                  <h5>Education</h5>
+                  <p>
+                    <strong>UCM - Madrid</strong><br>
+                    Fine Arts<br>
+                    <strong>ITA - Zaragoza</strong><br>
+                    User Experience Design
+                  </p>
+                </div>
+                <div class="column-2-3">
+                  <h5>Previous Experience</h5>
+                  <ul>
+                    <li>Plastic SCM - Valladolid, Spain</li>
+                    <li>Homestays - Dublin, Ireland</li>
+                  </ul>
+                </div>
+              </div><!-- / details -->
+            </div>
+          </div><!-- / row -->
+        </div><!-- / one-centered-column -->
+      </div><!-- / content -->
+    </div><!-- / section -->
   </section>
 </template>
+
+<style>
+  .media{
+    border-bottom: 1px solid rgb(196, 196, 196);
+    padding: 20px 0;
+  }
+  .media h5{
+    font-family: "Merriweather", times, serif;
+    font-weight: 300;
+    font-size: 20px;
+    line-height: 1.1em;
+    margin-bottom: 1em;
+  }
+  .media h6{
+    font-family: 'Niramit', 'Helvetica Neue', Arial, sans-serif;
+    color: #e54a35;
+    margin-bottom: 1rem;
+    font-size: 13px;
+    font-weight: 700;
+    letter-spacing: 1px;
+  }
+  .media p{
+    font-size: 14px;
+    margin-bottom: 1.5em;
+  }
+
+</style>
--- pages/blog.vue
+++ pages/blog.vue
@@ -13,27 +13,3 @@
     </div>
   </section>
 </template>
-
-<style scoped>
-  .hero-second-level {
-    background-size: cover;
-    background-position: center;
-    text-align: center;
-    padding: 6vw 8vw;
-    margin-bottom: 70px;
-  }
-  .hero-second-level h1 {
-    font-family: "Merriweather", times, serif;
-    font-weight: 300;
-    font-size: 4.5vw;
-    line-height: 1.1em;
-  }
-  .hero-second-level h4 {
-    font-family: 'Niramit', 'Helvetica Neue', Arial, sans-serif;
-    color: #e54a35;
-    margin-bottom: 1rem;
-    font-size: 13px;
-    font-weight: 700;
-    letter-spacing: 2px;
-  }
-</style>
