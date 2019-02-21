d474df61dc96594e437548df0ecc915b0b6a02bd
Fernando Val <aaromnido@gmail.com>
2019-02-21 18:37:59 +0100

    About page completed

--- pages/about.vue
+++ pages/about.vue
@@ -63,22 +63,20 @@
               <img src="https://static1.squarespace.com/static/590f9f02ebbd1a0ad377f7ba/t/59c31f2664b05fb1d241372e/1505959718275/craig.jpg?format=176w" alt="Patrick Lu" class="img-responsive" width="176">
             </div>
             <div class="column-3-4">
-              <h6>CEO / TECH LEAD</h6>
-              <h5>Patrick Lu</h5>
-              <p>I've previously worked for Apple as a software engineer and then went through the YCombinator Fellowship program building out my own businesses. I've built out apps full stack using React, Node, Django, and Java. I know what it takes to build a product from 0 to MVP and then from MVP to scale and I can do it quickly. I've led tech teams as large as 7.</p>
+              <h5>Craig Lu</h5>
+              <p>I've previously worked for Amazon as a software engineer. I have experience building apps using React, Node, Django, and Java. I love building beautiful user interfaces with intuitive user interaction.</p>
               <div class="row details">
                 <div class="column-1-3">
                   <h5>Education</h5>
                   <p>
-                    <strong>UC Berkeley</strong><br>
-                    Computer Science
+                    <strong>University of Florida</strong><br>
+                    Computer Engineering
                   </p>
                 </div>
                 <div class="column-2-3">
                   <h5>Previous Experience</h5>
                   <ul>
-                    <li>Apple Software Engineer in Machine Learning</li>
-                    <li>YCombinator Fellowship</li>
+                    <li>Amazon Software Engineer</li>
                   </ul>
                 </div>
               </div><!-- / details -->
@@ -93,7 +91,7 @@
             <div class="column-3-4">
               <h6>CEO / TECH LEAD</h6>
               <h5>Joey Pereira</h5>
-              <p>I've previously worked for Apple as a software engineer and then went through the YCombinator Fellowship program building out my own businesses. I've built out apps full stack using React, Node, Django, and Java. I know what it takes to build a product from 0 to MVP and then from MVP to scale and I can do it quickly. I've led tech teams as large as 7.</p>
+              <p>Joey has previously worked at Google and SmileyGo -- a startup with a successful exit. He's built apps using React, Django, Java, Ruby, and more. He loves thinking about hard backend problems and going deep into the low level stack to solve problems.</p>
               <div class="row details">
                 <div class="column-1-3">
                   <h5>Education</h5>
@@ -105,15 +103,15 @@
                 <div class="column-2-3">
                   <h5>Previous Experience</h5>
                   <ul>
-                    <li>Apple Software Engineer in Machine Learning</li>
-                    <li>YCombinator Fellowship</li>
+                    <li>SmileyGo</li>
+                    <li>Google</li>
                   </ul>
                 </div>
               </div><!-- / details -->
             </div>
           </div><!-- / row -->
           
-          <h2>Software Painter</h2>
+          <h2>Full Stack Designer</h2>
           <div class="row media">
             <div class="column-1-4">
               <img src="https://pbs.twimg.com/profile_images/987222302906568704/XzFfyUdb_400x400.jpg" alt="Patrick Lu" class="img-responsive" width="176">
@@ -121,7 +119,8 @@
             <div class="column-3-4">
               <h6>UX/UI Engineer</h6>
               <h5>Fernando Val</h5>
-              <p>I've previously worked for Apple as a software engineer and then went through the YCombinator Fellowship program building out my own businesses. I've built out apps full stack using React, Node, Django, and Java. I know what it takes to build a product from 0 to MVP and then from MVP to scale and I can do it quickly. I've led tech teams as large as 7.</p>
+              <p>I'm an experienced web designer. A renaissance kind of unicorn that is capable of creating a brand, draw illustrations, design a product from user experience to a user interface and build whole the complete Front End. Even in some cases, I'm capable to craft the backend.<br>
+              I'm a passionate person, a team player, husband and father of two kids, as well a Tesla lover, Celtic music melomaniac,  soccer mad supporter, and also a former soccer player.</p>
               <div class="row details">
                 <div class="column-1-3">
                   <h5>Education</h5>
@@ -140,6 +139,25 @@
                   </ul>
                 </div>
               </div><!-- / details -->
+              <div class="row details">
+                <div class="column-3-3">
+                  <h5>Keep in touch</h5>
+                  <ul>
+                    <li>
+                      <a href="http://fernandoval.es/" target="_blank">Portfolio</a>
+                    </li>
+                    <li>
+                      <a href="https://www.linkedin.com/in/aaromnido/" target="_blank">Linkedin</a>
+                    </li>
+                    <li>
+                      <a href="https://dribbble.com/aaromnido" target="_blank">Dribbble</a>
+                    </li>
+                    <li>
+                      <a href="mailto:aaromnido@gmail.com">Email</a>
+                    </li>
+                  </ul>
+                </div>
+              </div><!-- / details -->
             </div>
           </div><!-- / row -->
         </div><!-- / one-centered-column -->
@@ -152,6 +170,9 @@
   .media{
     border-bottom: 1px solid rgb(196, 196, 196);
     padding: 20px 0;
+  }
+  .media:last-child{
+    border-bottom: none;
   }
   .media h5{
     font-family: "Merriweather", times, serif;
@@ -171,6 +192,13 @@
   .media p{
     font-size: 14px;
     margin-bottom: 1.5em;
+  }
+  .details ul{
+    padding-left: 1.5em;
+  }
+  .details li,
+  .details p{
+    font-size: 13px;
   }
 
 </style>
